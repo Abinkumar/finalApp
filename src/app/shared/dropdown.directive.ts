1dd7d04ee9c3aa1468155ce582d801e711c3caa2
Abin Chetani <abin.chetani@gmail.com>
2017-08-07 16:03:19 +0545

    changes in directives
    chnages made in directives

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive,HostListener,HostBinding } from '@angular/core';
 
 @Directive({//decorator to say its a directive
-  selector: '[appDropdown]'
+  selector: '[appDropdown]'//attribute selector
 })
 export class DropdownDirective {
+ @HostBinding('class.open') isOpen=false;//adding css class open and the rest is handle by angular
+  
+@HostListener('click') toggleOpen(){
+  this.isOpen=!this.isOpen;
 
-  constructor() { }
+}
+  constructor() { } 
 
 }
